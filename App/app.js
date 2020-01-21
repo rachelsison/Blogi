bede50096134eb3629d5aa6837e242ed81a049d1
Rachel Sison <rsison87@gmail.com>
2020-01-21 10:20:27 -0500

    adding simple node server and connecting app to server

--- App/app.js
+++ App/app.js
@@ -1,1 +1,11 @@
 // App basics connecting the server to the application
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+module.exports = app;
--- App/server.js
+++ App/server.js
@@ -1,1 +1,6 @@
 // Simple node server
+const app = require('./App');
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
